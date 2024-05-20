6336a929da45600deeda3cd894e8b224a07d9064
hangyu <db0984@naver.com>
2024-05-20 09:49:36 +0900

    1

---
+++ prjTest/src/main/java/kr/ac/deu/tst/PrjTestApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.deu.tst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjTestApplication.class, args);
+	}
+
+}
---
+++ prjTest/src/test/java/kr/ac/deu/tst/PrjTestApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.deu.tst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
